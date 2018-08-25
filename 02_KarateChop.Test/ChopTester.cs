75c33d60785b8684fd6033bbb0477d52f20e61ad
Jorge Berlanga <jorge.berlanga@hotmail.com>
2018-08-25 14:25:24 -0700

    Kata 2. Sorted Arrays only.

---
+++ 02_KarateChop.Test/ChopTester.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata02_KarateChop;
+
+namespace Kata02_KarateChop.Test
+{
+    [TestClass]
+    public class ChopTester
+    {
+        private Chop chop;
+        [TestInitialize]
+        public void ChopTesterInit()
+        {
+            chop = new Chop();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(EmptyArrayException))]
+        public void EmptyArray_ThrowsError()
+        {            
+            Assert.AreEqual(1, chop.Chopper(1, new int[] {}));
+        }
+
+        [TestMethod]
+        public void SortedArrayTests_ReturnValidResponse()
+        {   
+            Assert.AreEqual(0, chop.Chopper(1, new[] { 1 }));
+            Assert.AreEqual(1, chop.Chopper(2, new[] { 1, 2 }));
+            Assert.AreEqual(2, chop.Chopper(3, new[] { 1, 2, 3, 4, 5 }));
+            Assert.AreEqual(3, chop.Chopper(3, new[] { -10, -1, 2, 3, 4, 5 }));
+            Assert.AreEqual(1, chop.Chopper(3, new []{ 1, 3, 6, 7, 8, 9, 10, 22, 33, 44}));
+            Assert.AreEqual(8, chop.Chopper(33, new[] { 1, 3, 6, 7, 8, 9, 10, 22, 33, 44 }));
+        }
+        /*
+        [TestMethod]
+        public void UnsortedArraytests_ReturnValidResponse()
+        {
+            //TODO: Unsorted Array support
+            //Assert.AreEqual(6, chop.Chopper(-7, new[] { 1, 2, 3, 4, 5, -7 }));
+        }*/
+        [TestMethod]
+        public void ValidArrayWithNoTarget_ThrowsNotFoundError()
+        {
+            Assert.ThrowsException<TargetNumberNotFoundException>(() => chop.Chopper(2, new[] { 1 }));
+            Assert.ThrowsException<TargetNumberNotFoundException>(() => chop.Chopper(0, new[] { 1, 2, 3, 4, 8, 10 }));
+            Assert.ThrowsException<TargetNumberNotFoundException>(() => chop.Chopper(-1, new[] { 3, 4, 8, 10 }));
+        }
+    }
+}
---
+++ 02_KarateChop/Chop.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata02_KarateChop
+{
+    public class Chop
+    {
+        public int Chopper(int target, int[] theArray)
+        {
+            if (theArray.Length == 0)
+                throw new EmptyArrayException("The array is empty");
+            int first = 0, last = theArray.Length - 1, middle = 0, position = -1;
+            bool found = false;
+            int iterations = 0;
+            if(first == last)
+            {
+                if (theArray[middle] == target)
+                    position = middle;
+                found = true;
+            }
+
+            while (!found && first <= theArray.Length && last >= 0)
+            {
+                middle = (first + last) / 2;
+                if (theArray[middle] == target)
+                {
+                    found = true;
+                    position = middle;
+                }
+                else
+                {
+                    if (theArray[middle] < target)
+                        first = middle + 1;
+                    else
+                        last = middle - 1;
+                }
+
+                iterations++;
+                if (iterations > 1000)
+                    throw new CycleThresholdException(
+                        CurrentStateInString(target, theArray, first, last, middle, iterations));
+            }
+
+            if (position >= 0)
+                return position;
+            else
+                throw new TargetNumberNotFoundException(
+                    CurrentStateInString(target, theArray, first, last, middle, iterations));
+        }
+
+        private static string CurrentStateInString(int target, int[] theArray, int first, int last, int middle, int iterations)
+        {
+            return $"First: {first}, Last: {last}, Middle: {middle}, " +
+                                    $"Iterations: {iterations}, Target: {target}, " +
+                                    $"ArrayCount: {theArray.Length.ToString()}.";
+        }
+    }
+
+    [Serializable]
+    public class CycleThresholdException : Exception
+    {
+
+
+        public CycleThresholdException()
+        {
+        }
+
+        public CycleThresholdException(string message) : base(message)
+        {
+        }
+
+        public CycleThresholdException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected CycleThresholdException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+    [Serializable]
+    public class TargetNumberNotFoundException : Exception
+    {
+
+
+        public TargetNumberNotFoundException()
+        {
+        }
+
+        public TargetNumberNotFoundException(string message) : base(message)
+        {
+        }
+
+        public TargetNumberNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected TargetNumberNotFoundException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+    [Serializable]
+    public class EmptyArrayException : Exception
+    {
+        public EmptyArrayException()
+        {
+        }
+
+        public EmptyArrayException(string message) : base(message)
+        {
+        }
+
+        public EmptyArrayException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected EmptyArrayException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
